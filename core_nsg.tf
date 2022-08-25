5d82273cb41e2bb792c69b80faf1efd8e12da7ba
Tom O'Connor <tom@twinhelix.org>
2022-08-26 00:33:14 +0100

    adding smtp_tls outbound.

--- core_nsg.tf
+++ core_nsg.tf
@@ -104,3 +104,35 @@ resource "oci_core_network_security_group_security_rule" "nsg_rule_https_out" {
     }
 
 }
+
+resource "oci_core_network_security_group_security_rule" "nsg_rule_smtptls_out" {
+    #Required
+    network_security_group_id = oci_core_network_security_group.main_network_security_group.id
+    direction = "EGRESS"
+    protocol = "6" //TCP
+
+    #Optional
+    description = "Allow SMTP_TLS Out"
+    destination = "0.0.0.0/0"
+    destination_type = "CIDR_BLOCK"
+    
+
+    source = var.cidr_block
+    source_type = "CIDR_BLOCK"
+    stateless = false
+    tcp_options {
+
+        #Optional
+        destination_port_range {
+            #Required
+            max = "587"
+            min = "587"
+        }
+        source_port_range {
+            #Required
+            max = "65535"
+            min = "1"
+        }
+    }
+
+}
